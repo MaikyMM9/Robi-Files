20c32c2d91559af852a02bd8ef1039337a5dd5d9
MaikyMM9 <51162002+MaikyMM9@users.noreply.github.com>
2020-06-20 13:30:33 +0200

    init
    init

--- index.js
+++ index.js
@@ -41,7 +41,10 @@ client.on("ready", async () => {
     client.user.setActivity('AnimalCrossing: NH', { type: 'PLAYING' })
         .then(presence => console.log(`Activiteit gezet naar: ${presence.activities[0].name}`))
         .catch(console.error);
-    client.user.setStatus('idle')
+    
+    
+    
+        client.user.setStatus('idle')
         .then(console.log)
         .catch(console.error);
 
