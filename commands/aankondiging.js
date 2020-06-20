3a552523c84112ac1837fc785e0d229a6274505a
MaikyMM9 <51162002+MaikyMM9@users.noreply.github.com>
2020-06-20 14:31:37 +0200

    init
    init

---
+++ commands/aankondiging.js
@@ -0,0 +1,47 @@
+const discord = require("discord.js")
+
+module.exports.run = async (client, message, arguments) => {
+
+    message.delete();
+
+
+    if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send(message.author + " Deze commando kun jij helaas niet gebruiken!")
+
+    
+
+    
+
+    var aankondiging = args.join(" ")
+
+    var aankondigingEmbed = new discord.MessageEmbed()
+        .setTitle("Aankondiging")
+        .setColor("#86ceb1")
+        .setDescription('Nieuwe aankoniging!')
+        .addField(`De aankondiging:`, aankondiging)
+        .setFooter("Robi")
+
+
+        var aankondigingChannel = message.guild.channels.find(`name`, "aankondigingen❗")
+
+        if(!aankondigingChannel) return message.author.send("Geen kanaal gevonden!")
+
+    aankondigingChannel.send(aankondigingEmbed)
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+}
+module.exports.help = {
+    name: "aankondiging"
+
+}
