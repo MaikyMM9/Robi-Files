06cc2f1191c3a27aa22b24724ef4920ac3c7d7bb
MaikyMM9 <51162002+MaikyMM9@users.noreply.github.com>
2020-06-20 22:17:41 +0200

    init
    init

--- commands/ping.js
+++ commands/ping.js
@@ -1,6 +1,12 @@
 module.exports.run = async (client, message, arguments) => {
 
 
+
+    if (message.member.roles.cache.has('17635063218438165')) {
+        return message.channel.send("Je beschikt niet over de Game-Pass")
+
+    }
+
     if (message.member.roles.cache.has('17635063218438165')) {
 
 
