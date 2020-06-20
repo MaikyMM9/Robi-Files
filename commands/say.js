e551c3ea76fcb149ceb00824c8d762cb5595a27c
MaikyMM9 <51162002+MaikyMM9@users.noreply.github.com>
2020-06-20 09:51:34 +0200

    init
    init

---
+++ commands/say.js
@@ -0,0 +1,20 @@
+const discord = require("discord.js")
+
+module.exports.run = async (bot, message, args) => {
+
+    if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send(message.author + "Deze commando is alleen voor de staff leden!")
+
+    var botMessage = args.join(" ")
+
+    message.delete();
+
+    message.channel.send(botMessage)
+
+
+
+
+}
+module.exports.help = {
+    name: "say"
+
+}
