618e4765148b474a5cafc3abda81703804544c22
MaikyMM9 <51162002+MaikyMM9@users.noreply.github.com>
2020-06-20 22:24:19 +0200

    init
    init

--- commands/hallo.js
+++ commands/hallo.js
@@ -2,7 +2,7 @@ const discord = require("discord.js")
 
 module.exports.run = async (client, message, arguments) => {
 
-    if (message.member.roles.cache.has('17635063218438165')) {
+    if (!message.member.roles.cache.has('17635063218438165')) {
         return message.channel.send("Je beschikt niet over de Game-Pass")
 
     }
